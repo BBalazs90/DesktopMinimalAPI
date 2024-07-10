09fd283000a1dac5c44ae7941e65780ece68b1cf
ala2bp <balazs.balogh2@hu.bosch.com>
2024-07-10 06:43:37 +0200

    Initial push.

---
+++ src/DesktopMinimalAPI/WebMessageBroker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopMinimalAPI;
+internal class WebMessageBroker
+{
+    private WebMessageBroker() {}
+
+    public static WebMessageBroker Create()
+    {
+        return new WebMessageBroker();
+    }
+}
