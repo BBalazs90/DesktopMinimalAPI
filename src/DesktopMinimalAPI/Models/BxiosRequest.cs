d0627408828cae39e27eaefa5dfc73c8c79fb727
ala2bp <balazs.balogh2@hu.bosch.com>
2024-07-15 10:49:31 +0200

    Initial commit.

---
+++ src/DesktopMinimalAPI/Models/BxiosRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopMinimalAPI.Models;
+
+public record BxiosRequest(string RequestId, string Method, string Path, string? Body = null);
---
+++ src/DesktopMinimalAPI/Models/BxiosResponse.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopMinimalAPI.Models;
+
+public record BxiosResponse(string RequestId, int Status, string Data);
