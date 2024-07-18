41e29a788fd15958a2dc4b5ce840decd8b4e96b6
ala2bp <balazs.balogh2@hu.bosch.com>
2024-07-18 17:23:26 +0200

    Attempts to implement async handler support.

--- src/DesktopMinimalAPI.Core/WebMessageBrokerCore.cs
+++ src/DesktopMinimalAPI.Core/WebMessageBrokerCore.cs
@@ -3,19 +3,24 @@ using Microsoft.Web.WebView2.Core;
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DesktopMinimalAPI;
 
 public sealed partial class WebMessageBrokerCore
 {
     internal readonly Dictionary<IRoute, Func<WmRequest, WmResponse>> MessageHandlers = new();
+    internal readonly Dictionary<IRoute, Func<WmRequest, Task<WmResponse>>> AsyncMessageHandlers = new();
 
     public readonly CoreWebView2 CoreWebView;
+    private readonly SynchronizationContext? _context;
 
     public WebMessageBrokerCore(CoreWebView2 coreWebView)
     {
         CoreWebView = coreWebView;
         CoreWebView.WebMessageReceived += OnWebMessageReceived;
+        _context = SynchronizationContext.Current;
     }
 
 
@@ -30,6 +35,26 @@ public sealed partial class WebMessageBrokerCore
             _ => null
         };
 
+        if (handler is null)
+        {
+            var asyncHandler = request.Method switch
+            {
+                Methods.Get => AsyncMessageHandlers.GetValueOrDefault((StringRoute)(request.Path)),
+                _ => null
+            };
+
+            Task.Run(async () => await asyncHandler?
+                .Invoke(request)
+                .ContinueWith(resp =>
+            {
+                _context.Post(_ =>
+                {
+                    CoreWebView?.PostWebMessageAsString(JsonSerializer.Serialize(resp.Result, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }));
+                }, null);
+
+            }));
+        }
+
         var response = handler?.Invoke(request);
 
 
@@ -40,15 +65,18 @@ public sealed partial class WebMessageBrokerCore
 
     public void MapGet(IRoute route, Func<WmRequest, WmResponse> handler)
     {
-        MessageHandlers.Add(route, handler);
+        MessageHandlers.Add(route, handler);           
+    }
 
-        var apply = (string r, Func<WmRequest, WmResponse> h) => MapGet((StringRoute)r, h);
-           
+    public void MapGet(IRoute route, Func<WmRequest, Task<WmResponse>> handler)
+    {
+        AsyncMessageHandlers.Add(route, handler);
     }
 
     public void MapGet(string route, Func<WmRequest, WmResponse> handler) => MapGet((StringRoute)route, handler);
-    public void MapGet(string route, Action handler) => MapGet((StringRoute)route, handler.ToWebMessageCommunicationForm());
-    public void MapGet<T>(string route, Func<T> handler) => MapGet((StringRoute)route, handler.ToWebMessageCommunicationForm());
+    public void MapGet(string route, Action handler) => MapGet((StringRoute)route, HandlerPipeline.Transform( handler));
+    public void MapGet<T>(string route, Func<T> handler) => MapGet((StringRoute)route, HandlerPipeline.Transform(handler));
+    public void MapGet<T>(string route, Func<Task<T>> handler) => MapGet((StringRoute)route, HandlerPipeline.Transform(handler));
 
 }
 
@@ -96,10 +124,19 @@ public static class HandlerPipeline
               return new WmResponse(request.RequestId, 500, JsonSerializer.Serialize(ex));
           }
       };
+
+    public static Func<WmRequest, Task<WmResponse>> Transform<T>(Func<Task<T>> handler) =>
+      (request) =>
+      {
+          try
+          {
+              var result = handler();
+              return result.ContinueWith(t => new WmResponse(request.RequestId, 200, JsonSerializer.Serialize(t.Result)));
+          }
+          catch (Exception ex)
+          {
+              return Task.FromResult(new WmResponse(request.RequestId, 500, JsonSerializer.Serialize(ex)));
+          }
+      };
 }
 
-public static class ActionAndFuncExtensions
-{
-    public static Func<WmRequest, WmResponse> ToWebMessageCommunicationForm(this Action action) => HandlerPipeline.Transform(action);
-    public static Func<WmRequest, WmResponse> ToWebMessageCommunicationForm<T>(this Func<T> func) => HandlerPipeline.Transform(func);
-}
