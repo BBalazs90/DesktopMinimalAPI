d1919fea0a3ea8fdb9bb818d6c08421c2e197b6a
ala2bp <balazs.balogh2@hu.bosch.com>
2024-08-22 00:24:27 +0200

    Starts the foundation of url parameter handling. No error handling yet.

--- src/DesktopMinimalAPI.Core/RequestHandling/Models/Route.cs
+++ src/DesktopMinimalAPI.Core/RequestHandling/Models/Route.cs
@@ -1,17 +1,31 @@
 ﻿using LanguageExt;
 using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
 
 namespace DesktopMinimalAPI.Core.RequestHandling.Models;
 public sealed class Route : IEquatable<Route>
 {
-    public static Option<Route> From(string? path) =>
-        !string.IsNullOrWhiteSpace(path) && path.StartsWith('/')
-            ? new Route(path)
-            : Option<Route>.None;
+    public static Option<Route> From(string? maybeRoute) => maybeRoute is not null && maybeRoute.StartsWith('/')
+        ? new Route(GetPath(maybeRoute), GetParameters(maybeRoute))
+        : Option<Route>.None;
 
-    private Route(string path) => Path = new PathString(path);
+    static PathString GetPath(string route) => route.Contains('?', StringComparison.Ordinal)
+        ? new(string.Concat(route.TakeWhile(c => c != '?')))
+        : new(route);
+    static ImmutableArray<UrlParameterString> GetParameters(string route) => route.Contains('?', StringComparison.Ordinal)
+        ? route.Split('?')[1]
+        .Split('&')
+        .Aggregate(ImmutableArray<UrlParameterString>.Empty, (accu, paramPart) => accu.Add(new(paramPart.Split('=')[1])))
+        : [];
+
+    private Route(PathString path, ImmutableArray<UrlParameterString> parameters) =>
+        (Path, Parameters) = (path, parameters);
 
     public PathString Path { get; }
+
+    public ImmutableArray<UrlParameterString> Parameters { get; }
 
     public override int GetHashCode() => Path.GetHashCode();
 
@@ -31,4 +45,17 @@ public readonly record struct PathString(string Value)
 
     public override string ToString() => Value;
     public static implicit operator string(PathString rootString) => rootString.ToString();
+}
+
+public readonly record struct UrlParameterString(string Value)
+{
+    public UrlParameterString() : this(string.Empty) { } // Throws
+
+    public string Value { get; } =
+        !string.IsNullOrWhiteSpace(Value)
+            ? Value
+            : throw new ArgumentException("Value cannot be null or whitespace.", nameof(Value));
+
+    public override string ToString() => Value;
+    public static implicit operator string(UrlParameterString parameter) => parameter.ToString();
 }
